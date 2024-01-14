44b53a5f20363ca22cd8ccb25b2b5c5cb500b9f1
ZubairImtiaz3 <zubairimtiaz395@gmail.com>
2024-01-15 04:40:27 +0500

    Implemented login

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,139 @@
+"use client";
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/ui/icons";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
+import { useEffect } from "react";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {
+  email: string;
+  password: string;
+  searchParams: { message?: string };
+  onSignIn?: (data: { email: string; password: string }) => Promise<void>;
+}
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
+
+export function UserAuthForm({
+  className,
+  email,
+  password,
+  onSignIn,
+  searchParams,
+  ...props
+}: UserAuthFormProps) {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [submissionCount, setSubmissionCount] = React.useState(0);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(validationSchema),
+  });
+
+  async function onSubmit(data: { email: string; password: string }) {
+    setIsLoading(true);
+    if (onSignIn) {
+      await onSignIn({
+        email: data.email,
+        password: data.password,
+      });
+    }
+    setSubmissionCount((prevCount) => prevCount + 1);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if (searchParams.message) {
+      toast({
+        variant: "default",
+        title: searchParams.message,
+        description: "Please check your email and password.",
+      });
+    }
+  }, [searchParams.message, submissionCount]);
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-2">
+          <div className="grid gap-2">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              {...register("email")}
+              id="email"
+              name="email"
+              placeholder="Enter Address"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              {...register("password")}
+              name="password"
+              id="password"
+              placeholder="Password"
+              type="password"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            )}
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading ? (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            ) : (
+              "Sign In"
+            )}
+          </Button>
+        </div>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <Button variant="outline" type="button" disabled={isLoading}>
+        {isLoading ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.google className="mr-2 h-4 w-4" />
+        )}{" "}
+        Google
+      </Button>
+    </div>
+  );
+}
