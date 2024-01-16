7b9d87693bc7a5572b0aaa5314520e0fde5fc1ef
ZubairImtiaz3 <zubairimtiaz395@gmail.com>
2024-01-16 17:55:05 +0500

    Fix 505 for protected routes

--- components/auth/WithAuth.tsx
+++ components/auth/WithAuth.tsx
@@ -1,8 +1,8 @@
 import { checkAuth } from "@/utils/supabase/session";
 
 const withAuth = (WrappedComponent: any) => {
-  const WithAuthComponent = (props: any) => {
-    checkAuth();
+  const WithAuthComponent = async (props: any) => {
+    await checkAuth();
 
     // Render the wrapped component if authenticated
     return <WrappedComponent {...props} />;
