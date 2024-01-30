1a3f13cb0cd4918ec87ecb65f44a7fdae82f5bd1
ZubairImtiaz3 <zubairimtiaz395@gmail.com>
2024-01-30 23:56:16 +0500

    update invoice

--- components/dashboard/tabs-content/invoice.tsx
+++ components/dashboard/tabs-content/invoice.tsx
@@ -1,6 +1,7 @@
-import { cookies } from "next/headers";
-import { createClient } from "@/utils/supabase/server";
+"use client";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
+
 import {
   Card,
   CardContent,
@@ -14,92 +15,116 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { InvoiceTable } from "./invoice-table";
 
-export async function Invoice() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  const { data } = await supabase.from("profiles").select().eq("id", user?.id);
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 
-  const companyData = data && data[0] ? data[0] : null;
+const schema = yup.object().shape({
+  invoiceBy: yup.string().required("Invoice by is required"),
+  customerName: yup.string().required("Customer Name is required"),
+});
+
+export function Invoice() {
+  const companyData = null;
+
+  //Process Invoice Data
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
 
   return (
-    <Card>
-      <CardHeader>
-        <div className="flex gap-4 justify-between items-center flex-wrap">
-          <div className="grow">
-            <Label htmlFor="subject">Invoice#</Label>
-            <Input id="subject" placeholder="Invoice Number" />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Card>
+        <CardHeader>
+          <div className="flex gap-4 justify-between items-center flex-wrap">
+            <div className="grow">
+              <Label htmlFor="invoice#">Invoice#</Label>
+              <Input placeholder="Invoice Number" />
+            </div>
+            <div className="grow">
+              <Label htmlFor="invoiceBy">Invoice By</Label>
+              <Input {...register("invoiceBy")} placeholder="Invoicer's Name" />
+              <p className="text-red-500 text-sm mt-1">
+                {errors.invoiceBy?.message}
+              </p>
+            </div>
+            <div className="flex flex-col justify-end gap-1 grow">
+              <Label htmlFor="date">Date</Label>
+              <DatePicker />
+            </div>
           </div>
-          <div className="grow">
-            <Label htmlFor="subject">Invoice By</Label>
-            <Input id="subject" placeholder="Invoicer's Name" />
-          </div>
-          <div className="flex flex-col justify-end gap-1 grow">
-            <Label htmlFor="subject">Date</Label>
-            <DatePicker />
-          </div>
-        </div>
-      </CardHeader>
+        </CardHeader>
 
-      <CardContent className="flex flex-col gap-6">
-        <CardTitle>Company's Information</CardTitle>
+        <CardContent className="flex flex-col gap-6">
+          <CardTitle>Company's Information</CardTitle>
 
-        <div className="flex gap-4 items-center flex-wrap">
-          <div className="grow">
-            <Label htmlFor="subject">Name</Label>
-            <Input
-              disabled={companyData?.company_name}
-              value={companyData?.company_name}
-              id="subject"
-              placeholder="Enter your company name"
+          <div className="flex gap-4 items-center flex-wrap">
+            <div className="grow">
+              <Label htmlFor="companyName">Name</Label>
+              <Input
+                disabled={companyData?.company_name}
+                value={companyData?.company_name}
+                placeholder="Enter your company name"
+              />
+            </div>
+            <div className="grow">
+              <Label htmlFor="companyPhoneNumber">Phone Number</Label>
+              <Input
+                disabled={companyData?.company_phone}
+                value={companyData?.company_phone}
+                placeholder="Enter your company phone"
+              />
+            </div>
+          </div>
+          <div>
+            <Label htmlFor="companyAddress">Address</Label>
+            <Textarea
+              disabled={companyData?.company_address}
+              value={companyData?.company_address}
+              placeholder="Company's Address. City, State, Zip, Country"
             />
           </div>
-          <div className="grow">
-            <Label htmlFor="subject">Phone Number</Label>
-            <Input
-              disabled={companyData?.company_phone}
-              value={companyData?.company_phone}
-              id="subject"
-              placeholder="Enter your company phone"
-            />
+          <CardTitle>Customer's Information</CardTitle>
+          <div className="flex gap-4 items-center flex-wrap">
+            <div className="grow">
+              <Label htmlFor="customerName">Name</Label>
+              <Input
+                {...register("customerName")}
+                id="subject"
+                placeholder="Enter you customer's name"
+              />
+              <p className="text-red-500 text-sm mt-1">
+                {errors.customerName?.message}
+              </p>
+            </div>
+            <div className="grow">
+              <Label htmlFor="customerPhoneNumber">Phone Number</Label>
+              <Input placeholder="Enter you customer's phone" />
+            </div>
           </div>
-        </div>
-        <div>
-          <Label htmlFor="description">Address</Label>
-          <Textarea
-            disabled={companyData?.company_address}
-            value={companyData?.company_address}
-            id="description"
-            placeholder="Company's Address. City, State, Zip, Country"
-          />
-        </div>
-        <CardTitle>Customer's Information</CardTitle>
-        <div className="flex gap-4 items-center flex-wrap">
-          <div className="grow">
-            <Label htmlFor="subject">Name</Label>
-            <Input id="subject" placeholder="Enter you customer's name" />
+          <div>
+            <Label htmlFor="customerAddress">Address</Label>
+            <Textarea placeholder="Customer's Address. City, State, Zip, Country" />
           </div>
-          <div className="grow">
-            <Label htmlFor="subject">Phone Number</Label>
-            <Input id="subject" placeholder="Enter you customer's phone" />
-          </div>
-        </div>
-        <div>
-          <Label htmlFor="description">Address</Label>
-          <Textarea
-            id="description"
-            placeholder="Customer's Address. City, State, Zip, Country"
-          />
-        </div>
-        <CardTitle>Sales Summary</CardTitle>
-        <InvoiceTable />
-      </CardContent>
-      <CardFooter className="justify-between space-x-2">
-        <Button variant="ghost">Cancel</Button>
-        <Button>Generate Sale</Button>
-      </CardFooter>
-    </Card>
+          <CardTitle>Sales Summary</CardTitle>
+          <InvoiceTable />
+        </CardContent>
+        <CardFooter className="justify-between space-x-2">
+          <Link href="/">
+            <Button variant="ghost">Cancel</Button>
+          </Link>
+
+          <Button type="submit">Generate Sale</Button>
+        </CardFooter>
+      </Card>
+    </form>
   );
 }
